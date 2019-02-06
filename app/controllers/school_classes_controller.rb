d5eac3c66699d1d8e9f7a814268a4220be99cebd
Elise Phan <aliceconnic@gmail.com>
2019-02-06 22:38:08 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -11,15 +11,15 @@ class SchoolClassesController < ApplicationController
   end
   
   def show
-    @school_class = SchoolClass.find(params[:id])
+    @school_class = SchoolClass.find(params[:school_class][:id])
   end
   
   def edit
-    @school_class = SchoolClass.find(params[:id])
+    @school_class = SchoolClass.find(params[:school_class][:id])
   end
   
   def update
-    @school_class = SchoolClass.find(params[:id])
+    @school_class = SchoolClass.find(params[:school_class][:id])
     @school_class.update(school_class_params(:title))
     redirect_to school_class_path(@school_class)
   end
