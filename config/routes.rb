fce0558d47cceac8cc491ea293673559196265de
Elise Phan <aliceconnic@gmail.com>
2019-02-06 22:56:55 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :students, only: [:index,:show,:new,:create,:edit]
   resources :school_classes, only: [:index,:show,:new,:create,:edit]
-  
+  get 'students/:id/edit', to: 'students#edit', as: :edit_student
+  get 'school_classes/:id/edit', to: 'school_classes#edit', as: :edit_school_class
 end
