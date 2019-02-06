cb3596041b27fd158e70392f19e55e86fb1690ff
Elise Phan <aliceconnic@gmail.com>
2019-02-06 22:02:40 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_school_classes.rb
+++ db/migrate/002_create_school_classes.rb
@@ -2,7 +2,7 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :school_classes do |t|
       t.string :title
-      t.string :room_number
+      t.integer :room_number
 
       t.timestamps null: false
     end
